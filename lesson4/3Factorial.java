271c28a79daa36d922569213ffb53e167639ca38
Boris Siniak <35667330+siniakboris@users.noreply.github.com>
2018-01-22 23:43:24 +0200

    Create 3Factorial.java

---
+++ lesson4/3Factorial.java
@@ -0,0 +1,23 @@
+package com.gmail.siniakboris;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Введите число от 4 до 16");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int r = 1;
+
+		if ((n >= 4) | (n <= 16)) {
+			for (int i = 2; n >= i; i = i + 1) {
+				r = r * i;
+			} System.out.println("Факториал числа " + n +" равен " + r);
+
+		} else {
+			System.out.println("Ошибка, вы ввели неверное число.");
+		}
+	}
+}
