8fd1c60d25303a8a9f5d67f942babf3b14ee20cf
Boris Siniak <35667330+siniakboris@users.noreply.github.com>
2018-01-23 00:17:29 +0200

    Update 4Figure.java

--- lesson4/4Figure.java
+++ lesson4/4Figure.java
@@ -7,20 +7,23 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Введите высоту фигуры");
-		int h = sc.nextInt();
 		System.out.println("Введите ширину фигуры");
 		int w = sc.nextInt();
+		System.out.println("Введите высоту фигуры");
+		int h = sc.nextInt();
 
-		for (int i = 1; i <= w; i = i + 1) {
-			for (int j = 1; j <= h; j = j + 1) {
-				if ((j == 1) || (j == h) || (i == 1) || (i == h)) {
+		for (int i = 1; i <= h; i = i + 1) {
+			for (int j = 1; j <= w; j = j + 1) {
+				if ((j == 1) || (j == w) || (i == 1) || (i == h)) {
 					System.out.print("*");
 				} else {
 					System.out.print(" ");
 				}
+
 			}
 			System.out.println();
 		}
+
 	}
+
 }
