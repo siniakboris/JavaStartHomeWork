1c56fd895a82208b8dc1f1a3e6aebca0bc1f5075
Boris Siniak <35667330+siniakboris@users.noreply.github.com>
2018-01-22 22:35:46 +0200

    Create 1Wallpaper.java

---
+++ lesson4/1Wallpaper.java
@@ -0,0 +1,30 @@
+package com.gmail.siniakboris;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Введите количество полос");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = 5;
+
+		if (a > 0) {
+			for (int n = 1; n <= b; n = n + 1) {
+				for (int j = 1; j <= a; j = j + 1) {
+					if (j % 2 != 0) {
+						System.out.print("***");
+					} else {
+						System.out.print("+++");
+					}
+				}
+				System.out.println();
+			}
+		} else {
+			System.out.println("Ошибка, введите число больше 0");
+		}
+	}
+
+}
