19d3e300ade8a970b0221cee8fb41e16000f4dae
Boris Siniak <35667330+siniakboris@users.noreply.github.com>
2018-01-30 16:06:05 +0200

    Update 4MethodFindNumber.java

--- lesson6/4MethodFindNumber.java
+++ lesson6/4MethodFindNumber.java
@@ -18,6 +18,7 @@ public class FindNumber {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Введите число:");
 		int n = sc.nextInt();
 		int[] arrayOne = { 0, 1, 2, 2, 4, 5, 6, 7, 8, 9, 9, 1 };
 		System.out.println(methodFindNumber(arrayOne, n));
