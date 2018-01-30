89f655351c5fd4a1e1be4db6712f49f9d216947e
Boris Siniak <35667330+siniakboris@users.noreply.github.com>
2018-01-30 15:21:59 +0200

    Create 4MethodFindNumber.java

---
+++ lesson6/4MethodFindNumber.java
@@ -0,0 +1,27 @@
+package com.gmail.siniakboris;
+
+import java.util.Scanner;
+
+public class FindNumber {
+
+	public static int methodFindNumber(int[] array, int a) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == a) {
+				a = i;
+				return a;
+			}
+
+		}
+		return a = -1;
+
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arrayOne = { 0, 1, 2, 2, 4, 5, 6, 7, 8, 9, 9, 1 };
+		System.out.println(methodFindNumber(arrayOne, n));
+
+	}
+
+}
